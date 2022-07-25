18bb083a2489d57880b41907a8f763019545ce91
deliminzoo <minjoo9903@naver.com>
2022-07-25 22:39:00 +0900

    feat: image upload

--- drf/src/pages/UpLoadMain.js
+++ drf/src/pages/UpLoadMain.js
@@ -180,6 +180,43 @@ function UpLoadMain() {
 		var postId = id;
 		navigate(`/comment/${postId}`, { state: { postId: postId } });
 	};
+	const [image, setImage] = useState({
+		image_file: '',
+		preview_URL: 'img/default_image.png',
+	});
+
+	let inputRef;
+
+	const saveImage = (e) => {
+		e.preventDefault();
+		const fileReader = new FileReader();
+
+		if (e.target.files[0]) {
+			fileReader.readAsDataURL(e.target.files[0]);
+		}
+		fileReader.onload = () => {
+			setImage({
+				image_file: e.target.files[0],
+				preview_URL: fileReader.result,
+			});
+		};
+	};
+
+	const sendImageToServer = async () => {
+		if (image.image_file) {
+			const formData = new FormData();
+			formData.append('file', image.image_file);
+			await axios.post('/api/image/upload', formData);
+			alert('서버에 등록이 완료되었습니다!');
+			setImage({
+				image_file: '',
+				preview_URL: 'img/default_image.png',
+			});
+		} else {
+			alert('사진을 등록하세요!');
+		}
+	};
+
 	return (
 		<>
 			<GlobalStyle />
@@ -199,8 +236,21 @@ function UpLoadMain() {
 						<button>작성</button>
 					</form>
 					<div className="upload">
-						<button className="picturebtn"></button>
-						<button className="uploadbtn"></button>
+						<input
+							type="file"
+							accept="image/*"
+							onChange={saveImage}
+							// 클릭할 때 마다 file input의 value를 초기화 하지 않으면 버그가 발생할 수 있다
+							// 사진 등록을 두개 띄우고 첫번째에 사진을 올리고 지우고 두번째에 같은 사진을 올리면 그 값이 남아있음!
+							onClick={(e) => (e.target.value = null)}
+							ref={(refParam) => (inputRef = refParam)}
+							style={{ display: 'none' }}
+						/>
+						<button
+							className="picturebtn"
+							onClick={() => inputRef.click()}
+						></button>
+						<button className="uploadbtn" onClick={sendImageToServer}></button>
 					</div>
 					{posts.map((post) => {
 						return (
